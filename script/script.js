44bced845437cca720d64b7ee0a3f8b699271560
TigranPoghos <tigran2002poghosyan@gmail.com>
2025-04-12 16:12:55 +0300

    correct

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     
     var swiper = new Swiper(".mySwiper", {
       slidesPerView: 3,
+      spaceBetween: 5,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
